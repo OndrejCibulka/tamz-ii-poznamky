d39f68b4edcefaaa10c8c9de9e23b92922c01f2d
Ondřej Cibulka <bezvamail@seznam.cz>
2017-11-20 18:52:59 +0100

    Initial commit

---
+++ tamz-ii-poznamky/app/src/test/java/com/example/cib0020/tamz_ii_poznamky/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.cib0020.tamz_ii_poznamky;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
