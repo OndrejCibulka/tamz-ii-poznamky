6d0219939ca89f9cac490970850f1b32b757b03a
Ondřej Cibulka <bezvamail@seznam.cz>
2017-11-27 15:37:13 +0100

    Přidané menu v hlavní aktivitě včetně smazat vše.

--- tamz-ii-poznamky/app/src/main/java/com/example/cib0020/tamz_ii_poznamky/MainActivity.java
+++ tamz-ii-poznamky/app/src/main/java/com/example/cib0020/tamz_ii_poznamky/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.cib0020.tamz_ii_poznamky;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+//import android.view.MenuItem.OnMenuItemClickListener;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -20,6 +25,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final int NOTE_DETAIL_REQUEST = 0;
+    DatabaseHandler db = null;
 
     List<Note> listNotes;
 
@@ -28,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseHandler db = new DatabaseHandler(this);
+        db = new DatabaseHandler(this);
         this.refreshList();
 
 //      Toast.makeText(this, note.getTitle(), Toast.LENGTH_LONG).show();
@@ -76,5 +82,43 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, your_array_list );
         lv.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.delete_all:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setCancelable(true);
+                builder.setTitle("Opravdu?");
+                builder.setMessage("Opravdu chece smazat všechny poznámky?");
+                builder.setPositiveButton("Ano, smazat",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                db.deleteAllNotes();
+                                MainActivity.this.refreshList();
+                            }
+                        });
+                builder.setNegativeButton("Zrušit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
